50a09c08a3cdb9b2324940df98d18d051004838e
Arjun Singh <mearjun1997roy@gmail.com>
2021-09-09 17:27:52 +0600

    Modified index file

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 /// Finding an Object Class
 
-
-function sum(...arguments) {
-  if (arguments.length === 1) {
-    const [fristArr] = arguments
-    if (fristArr instanceof Array) {
-      return sum(...fristArr)
-    }
-  }
-  return arguments.reduce((a,b)=> a + b)
-}
-
-console.log(sum(1))
-console.log(sum([1,2,3,4,5]))
-console.log(sum(4,2))
