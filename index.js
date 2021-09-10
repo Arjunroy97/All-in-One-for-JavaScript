c40bfbfd3c13d84a1cf7ce429f6f670ceef3167a
Arjun Singh <mearjun1997roy@gmail.com>
2021-09-10 11:00:08 +0600

    finding an object class

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-/// Finding an Object Class
 
+
+/* finding an Object class */
+
+function sum(...arguments) {
+    const [firstArr] = arguments;
+    if (firstArr instanceof Array) {
+        return sum(...firstArr);
+    }
+    return arguments.reduce((a,b)=> a+b)
+}
+const result = sum(1, 2, 3, 4);
+console.log(result)
